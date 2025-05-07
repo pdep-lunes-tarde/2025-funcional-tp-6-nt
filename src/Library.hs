f82839fe0cd16d654d16cbbf6d287d2a56848e7a
TotoSassani <tsassani@frba.utn.edu.ar>
2025-05-07 12:41:51 -0300

    hasta ejercicio parte 2

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,8 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 module Library where
 import PdePreludat
+import GHC.IO.Handle (hClose)
 
 data Ingrediente =
     Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
@@ -18,3 +21,27 @@ data Hamburguesa = Hamburguesa {
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+precioTotal :: Hamburguesa -> Number
+precioTotal burguer = precioBase burguer + sumarPrecios (ingredientes burguer)
+
+sumarPrecios :: [Ingrediente] -> Number
+sumarPrecios [] = 0
+sumarPrecios (cabeza:cola) = precioIngrediente cabeza + sumarPrecios cola
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar burguer 
+    |contiene Carne (ingredientes burguer) = agregarIngrediente Carne burguer
+    |contiene Pollo (ingredientes burguer) = agregarIngrediente Pollo burguer
+    |otherwise = burguer
+
+contiene :: Ingrediente -> [Ingrediente] -> Bool
+contiene _ [] = False
+contiene ingrediente (cabeza:cola) 
+    |ingrediente == cabeza = True
+    |otherwise = contiene ingrediente cola
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa 
+agregarIngrediente ing (Hamburguesa precio ings) = Hamburguesa (precio + precioIngrediente ing) (ing:ings)
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento porcentajeDescuento (Hamburguesa precio ingredientes) = Hamburguesa (precio * (100 - porcentajeDescuento)/100) ingredientes 
--- src/Spec.hs
+++ src/Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NondecreasingIndentation #-}
 module Spec where
 import PdePreludat
 import Library
@@ -6,6 +7,21 @@ import Control.Exception (evaluate)
 
 correrTests :: IO ()
 correrTests = hspec $ do
-    describe "TP 5" $ do
-        it "test de prueba" $ do
-            2 + 2 `shouldBe` 4
+    describe "agrandar" $ do
+        it "cada vez que se agranda una hamburguesa se agrega otro ingrediente base, en este caso se agrega una carne" $ do
+            agrandar (Hamburguesa 20 [Carne]) `shouldBe` Hamburguesa 40 [Carne, Carne]
+        it "cada vez que se agranda una hamburguesa se agrega otro ingrediente base, en este caso se agrega un pollo" $ do
+            agrandar (Hamburguesa 10 [Pollo]) `shouldBe` Hamburguesa 20 [Pollo, Pollo]
+
+        describe "agregarIngrediente" $ do
+        it "recibe un ingrediente y lo agrega a la hamburguesa" $ do
+            agregarIngrediente Panceta (Hamburguesa 24 [Pan, Carne, Pan]) `shouldBe` Hamburguesa 34 [Panceta, Pan, Carne, Pan]
+
+        describe "descuento" $ do
+        it "aplica un descuento sobre el precio de la hamburguesa" $ do
+            descuento 50 (Hamburguesa 24 [Pan, Carne, Pan]) `shouldBe` Hamburguesa 12 [Pan, Carne, Pan]
+        it "aplica un descuento sobre el precio de la hamburguesa" $ do
+            descuento 25 (Hamburguesa 44 [Pan, Carne, Panceta, Cheddar, Pan]) `shouldBe` Hamburguesa 33 [Pan, Carne, Panceta, Cheddar, Pan]
+        
+
+      
